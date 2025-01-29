f01404b0eca4dee268d8219bc2ab7b724c20cff7
AnteData <florian.boehringer@antedata.ch>
2025-01-29 11:07:16 +0100

    Add files via upload

--- integrate.R
+++ integrate.R
@@ -1,7 +1,7 @@
 require(tidyr)
 require(xts)
 
-## Load CSV
+#### Load & prepare Data 
 df <- read.csv( "https://storage.googleapis.com/antedata_fine/101/AllDat.csv")
 
 # filter timeseries: source= "Aggregate", itemn= "TrendRank" 
@@ -13,7 +13,29 @@ df <- as.xts( df[,2:ncol(df)] , order.by=as.Date(df[,1]) )
 # use signal on fridays
 index(df) <- index(df) + 4
 
-# calculate backtest
-df2 <- btest(SIG,PRIX_orig, 0.2 ,lagger=1,comb=F, decay=F, tsiz=0.03)
+
+
+######### Backtest 
+SIG <- df
+
+# create long/short portfolio of best/worst ranked
+SIG[,] <- rowRanks( SIG, ties.method="random" )
+SIG[SIG < (0.2*ncol(SIG)) ] <- -0.03; SIG[SIG > (0.8*ncol(SIG)) ] <- 0.03; SIG[SIG>1] <- 0
+
+# harmonize columns of Signal and Price
+cn <- unique( colnames(SIG)[ which( colnames(SIG) %in% colnames(PRIX)) ] )
+SIG <- SIG[,cn]; PRIX <- PRIX[,cn]
+
+# calculate backtest returns
+OUT <- merge(stats::lag(SIG,1),PRIX, all=F)  # Achtung: stats::lag=4 (stats::lag = 2)  ; noch stats::lag -1 wenn 4 months holding
+for (col in seq(1: dim(SIG)[2]   )) {
+  OUT[,col] <- OUT[,col] * OUT[,col+dim(SIG)[2]] 
+}
+OUT <- tail( OUT[,1:dim(SIG)[2]] ,-3) 
+plot(cumsum(rowSums(OUT)))
+
+
+
+
 
 
