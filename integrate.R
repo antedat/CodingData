76993f3c38b3df042d93c44d1fa811616fce31ee
AnteData <florian.boehringer@antedata.ch>
2025-05-31 10:00:33 +0200

    Update integrate.R

--- integrate.R
+++ integrate.R
@@ -2,15 +2,10 @@ require(tidyr)
 require(xts)
 
 #### Load & prepare Data 
-df <- read.csv( "https://storage.googleapis.com/antedata_fine/101/AllDat.csv")
+df <- read.csv( "https://storage.googleapis.com/antedata_open/AllDat_excerpt_ticker.csv")
 
 # filter timeseries: source= "Aggregate", itemn= "TrendRank" 
 df <- spread( df[ which(df$source == "Aggregate"),c("date","ticker","TrendRank") ], key="ticker", value="TrendRank")
 
 # format as Timeseries
 df <- as.xts( df[,2:ncol(df)] , order.by=as.Date(df[,1]) )
-
-# use signal on fridays
-index(df) <- index(df) + 4
-
-
